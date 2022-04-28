e8fee5dc668ae4db5b592e0351bff50e65566922
Pedro-OSS117 <61086029+Pedro-OSS117@users.noreply.github.com>
2022-04-28 13:14:09 +0200

    First Commit

---
+++ Source/MyFirstGame/MyFirstGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyFirstGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyFirstGame, "MyFirstGame" );
