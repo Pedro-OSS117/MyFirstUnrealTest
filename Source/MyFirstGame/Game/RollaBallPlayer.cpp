6affc76fff4c9c34117c00f178e92fe298b33eb3
Pedro-OSS117 <61086029+Pedro-OSS117@users.noreply.github.com>
2022-04-29 18:00:51 +0200

    Premier step - IL BOUGE

--- Source/MyFirstGame/Game/RollaBallPlayer.cpp
+++ Source/MyFirstGame/Game/RollaBallPlayer.cpp
@@ -2,27 +2,42 @@
 
 
 #include "RollaBallPlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 ARollaBallPlayer::ARollaBallPlayer()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
+	// Create Components
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>("SpringArm");
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+
+	// Set the root component to be our Mesh
+	RootComponent = Mesh;
+
+	// Attach the SpringArm to the Mesh, Spring will now follow the mesh transform
+	SpringArm->SetupAttachment(Mesh);
+
+	// Attach Camera to the SpringArm, Camera will now follow the SpringArm transform
+	Camera->SetupAttachment(SpringArm);
+
+	Mesh->SetSimulatePhysics(true);
+
+	Mesh->OnComponentHit.AddDynamic(this, &ARollaBallPlayer::OnHit);
 }
 
 // Called when the game starts or when spawned
 void ARollaBallPlayer::BeginPlay()
 {
 	Super::BeginPlay();
-	
-}
+	MoveForce *= Mesh->GetMass();
+	JumpImpulse *= Mesh->GetMass();
 
-// Called every frame
-void ARollaBallPlayer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Orange, FString::Printf(TEXT("coucou "), MoveForce));
 }
 
 // Called to bind functionality to input
@@ -30,5 +45,40 @@ void ARollaBallPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	InputComponent->BindAxis("MoveForward", this, &ARollaBallPlayer::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &ARollaBallPlayer::MoveRight);
+	InputComponent->BindAction("Jump", IE_Pressed, this, &ARollaBallPlayer::Jump);
+
+}
+
+void ARollaBallPlayer::MoveRight(float Value)
+{
+	const FVector Right = Camera->GetRightVector() * MoveForce * Value;
+	Mesh->AddForce(Right);
+}
+
+void ARollaBallPlayer::MoveForward(float Value)
+{
+	const FVector Forward = Camera->GetForwardVector() * MoveForce * Value;
+	Mesh->AddForce(Forward);
+}
+
+void ARollaBallPlayer::Jump()
+{
+	if (JumpCount >= MaxJumpCount) { return; }
+
+	Mesh->AddImpulse(FVector(0, 0, JumpImpulse));
+
+	JumpCount++;
+}
+
+void ARollaBallPlayer::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	const float HitDirection = Hit.Normal.Z;
+
+	if (HitDirection > 0)
+	{
+		JumpCount = 0;
+	}
 }
 
--- Source/MyFirstGame/Game/RollaBallPlayer.h
+++ Source/MyFirstGame/Game/RollaBallPlayer.h
@@ -6,6 +6,9 @@
 #include "GameFramework/Pawn.h"
 #include "RollaBallPlayer.generated.h"
 
+class UCameraComponent;
+class USpringArmComponent;
+
 UCLASS()
 class MYFIRSTGAME_API ARollaBallPlayer : public APawn
 {
@@ -19,11 +22,37 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Definte Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* Mesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UStaticMeshComponent* Mesh2;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USpringArmComponent* SpringArm;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* Camera;
+
+	// VARIABLES //
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float MoveForce = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float JumpImpulse = 500.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		int32 MaxJumpCount = 1;
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+
+	// FUNCTIONS
+	void MoveRight(float Value);
+	void MoveForward(float Value);
+	void Jump();
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
+	int32 JumpCount = 0;
 };
